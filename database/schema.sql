d96e0748f4f61cd82b5e9d15cc305b02a8ae4542
SulimanMahjna <elriced700@gmail.com>
2025-11-10 12:00:09 +0300

    new schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,36 +1,3 @@
--- CREATE TABLE user (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     name Text,
---     email Text UNIQUE,
---     password Text,
---     created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
---     updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-
--- );
-
--- CREATE TABLE places (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     name Text UNIQUE NOT NULL,
---     description Text UNIQUE NOT NULL,
---     location Text UNIQUE NOT NULL,
---     featuers Text UNIQUE NOT NULL,
--- );
-
--- CREATE TABLE favorites (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
---     user_id Int,
---     CONSTRAINT fk_user
---         FOREIGN KEY (user_id) 
---         REFERENCES user (id)
---         ON DELETE CASCADE ,
-
---     place_id Int,
---     CONSTRAINT fk_places
---         FOREIGN KEY (place_id) 
---         REFERENCES places (id)
---         ON DELETE CASCADE
--- )
 
 CREATE TABLE "users" (
   "id" uuid PRIMARY KEY,
