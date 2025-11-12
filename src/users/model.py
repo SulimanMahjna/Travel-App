6d78f2bb038475f7d1de2891c86d4728dd229a93
SulimanMahjna <elriced700@gmail.com>
2025-11-12 14:07:34 +0300

    final

--- src/users/model.py
+++ src/users/model.py
@@ -1,16 +1,6 @@
-from sqlalchemy import Table, Column, String, Text, TIMESTAMP
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.sql import func
-from src.database.connection import metadata
-import uuid
+from pydantic import BaseModel
 
-users = Table(
-    "users",
-    metadata,
-    Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
-    Column("username", String),
-    Column("email", String),
-    Column("password", Text),
-    Column("created_at", TIMESTAMP, server_default=func.now()),
-    Column("udated_at", TIMESTAMP, server_default=func.now())
-)
+class Userrequest(BaseModel):
+    name: str
+    email: str
+    password: str
